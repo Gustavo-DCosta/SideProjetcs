ed9e0f502db88c583180cd99ea76354ac02e6f20
Gustavo Da Costa Pereira <gustagamerpereira@gmail.com>
2025-09-09 08:52:35 +0200

    Update chenillard.cpp

--- chenillard.cpp
+++ chenillard.cpp
@@ -1,53 +1,49 @@
-const int led1 = 6;
-const int led2 = 7;
-const int led3 = 8;
-const int led4 = 9;
-const int led5 = 10;
-const int led6 = 11;
-const int led7 = 12;
-const int led8 = 13;
+int leds[8] = { 6, 7, 8, 9, 10, 11, 12, 13 }; 
 
 const int btnStart = 2;
-
-const int btnStop = 5;
-
+const int btnStop = 5; 
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);
-  pinMode(led7, OUTPUT);
-  pinMode(led8, OUTPUT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
 
   pinMode(btnStart, INPUT_PULLUP);
+  pinMode(btnStop, INPUT_PULLUP);
 }
 
 void loop() {
-  int leds[8] = { 6, 7, 8, 9, 10, 11, 12, 13 };
-
   int reader = digitalRead(btnStart);
   int breaker = digitalRead(btnStop);
 
-  if (reader == 1) {
+  if (reader == 0) {
     for (int i = 0; i < 8; i++) {
+      if (breaker == 0) {
+        break;
+      }
       digitalWrite(leds[i], HIGH);
-      delay(300);
+      delay(1000);
       digitalWrite(leds[i], LOW);
-      delay(300);
+      delay(1000);
       Serial.println(i);
+      if (breaker == 0) {
+        break;
+      }
     }
-
     for (int i = 8; i > 0; i--) {
+      if (breaker == 0) {
+        break;
+      }
       digitalWrite(leds[i], HIGH);
-      delay(300);
+      delay(1000);
       digitalWrite(leds[i], LOW);
-      delay(300);
+      delay(1000);
       Serial.println(i);
+      if (breaker == 0) {
+        break;
+      }
     }
   }
 }
